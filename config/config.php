0b9bb7a59d69d995c5390a6bcb9f73df80eec6a7
alejandropeculio@gmail.com <alejandropeculio@gmail.com>
2018-11-14 21:43:59 -0300

    config servidor remoto

--- config/config.php
+++ config/config.php
@@ -1,7 +1,15 @@
 <?php
-	define("DB_HOST", "localhost");
+	//Servidor local
+	/*define("DB_HOST", "localhost");
 	define("DB_USR", "root");
 	define("DB_PASS", "");
 	define("DB_DB", "trampas_bd");
-	//define(DB_TYPE, "mysql");
+	//define(DB_TYPE, "mysql");*/
+
+	//Serividor remoto
+	define("DB_HOST", "localhost");
+	define("DB_USR", "u520566866_admin");
+	define("DB_PASS", "E9JH6C1N");
+	define("DB_DB", "u520566866_tramp");
+	
 ?>
