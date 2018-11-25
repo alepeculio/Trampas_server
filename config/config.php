32edf25398993f908c603ce8cbe4fc68dfe958cf
alejandropeculio@gmail.com <alejandropeculio@gmail.com>
2018-11-25 15:52:16 -0300

    config

--- config/config.php
+++ config/config.php
@@ -1,15 +1,15 @@
 <?php
 	//Servidor local
-	define("DB_HOST", "localhost");
+	/*define("DB_HOST", "localhost");
 	define("DB_USR", "root");
 	define("DB_PASS", "");
 	define("DB_DB", "trampas_bd");
 	//define(DB_TYPE, "mysql");*/
 
 	//Serividor remoto
-	/*define("DB_HOST", "localhost");
+	define("DB_HOST", "localhost");
 	define("DB_USR", "u520566866_admin");
 	define("DB_PASS", "E9JH6C1N");
-	define("DB_DB", "u520566866_tramp");*/
+	define("DB_DB", "u520566866_tramp");
 	
 ?>
